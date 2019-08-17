260f41c7e7ba4ebbfa72928446ede53ea49e7001
krishnapsd.thapaliya@gmail.com <krishnapsd.thapaliya@gmail.com>
2019-08-16 19:40:12 -0500

    Commit

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Guess_My_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            int hidden;
+            hidden = rand.Next(100);
+            while (true)
+            {
+                Console.Write("Enter the Random Number :");
+                int userGuess = Int32.Parse(Console.ReadLine());
+
+                if(userGuess==hidden)
+                {
+                    break;
+                }
+                else if (userGuess> hidden && userGuess>0 && userGuess<101)
+                {
+                    Console.WriteLine("Too High!");
+                }
+                else if (userGuess < hidden && userGuess > 0 && userGuess < 101)
+                {
+                    Console.WriteLine("Too Low!");
+                }
+                else
+                {
+                    Console.WriteLine("Number out the Range");
+                }
+               
+            }
+            Console.WriteLine("Congrats You Are The WINNER !");
+            Console.ReadLine();
+        }
+    }
+}
